ad1939dcc3c96c360d03c873f9f710338efe7672
Juliana Emmanuelli <julianaemmanuelli@julianas-mbp.home>
2019-02-25 22:03:36 -0500

    attempting to convert app. Need to resolve issues w expo

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import AppContainer from './containers/App';
-import { AlertProvider } from './containers/AlertContainer';
-import createStore from './redux';
-const { store, persistor } = createStore();
-export default class App extends React.PureComponent {
-  render() {
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <AlertProvider>
-            <AppContainer />
-          </AlertProvider>
-        </PersistGate>
-      </Provider>
-    );
-  }
-}
+import HybridApp from './src/App.js';
+
+const App = props => {
+  return <HybridApp />;
+};
+export default App;
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import AppContainer from '../containers/App';
+import { AlertProvider } from '../containers/AlertContainer';
+import createStore from '../redux';
+const { store, persistor } = createStore();
+export default class HybridApp extends React.PureComponent {
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <AlertProvider>
+            <AppContainer />
+          </AlertProvider>
+        </PersistGate>
+      </Provider>
+    );
+  }
+}
