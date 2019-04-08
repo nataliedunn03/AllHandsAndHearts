6f431202550d889fd64167f7c3cafd0aea3a4380
DalidaMartinez <dalidamartinez@gmail.com>
2019-04-07 20:48:40 -0400

    removed zooming into regions for map zoom bug

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -212,7 +212,7 @@ export default class MapScreen extends React.PureComponent {
       //this.mapViewRef.fitToElements(true); // this will fill all the markers, not ideal when we will have all the region on the map.
       */
       this.mapViewRef.fitToCoordinates(this.state.markersData, {
-        edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+        edgePadding: { top: 100, right: 100, bottom: 100, left: 100 },
         animated: true
       });
     }
