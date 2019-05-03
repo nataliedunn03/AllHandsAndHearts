04c379c71ca0614097b2a458727c093f5d4dbcf3
Juliana Emmanuelli <jremmanue@gmail.com>
2019-05-03 16:17:07 -0400

    up to node path breaking

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: [
+      'babel-preset-expo',
+      'module:metro-react-native-babel-preset',
+      'module:react-native-dotenv'
+    ]
+    //   env: {
+    //     development: {
+    //       plugins: [
+    //         '@babel/transform-react-jsx-source',
+    //       ]
+    //     }
+    //   }
+  };
+};
